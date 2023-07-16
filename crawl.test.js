cbd0893e762241c860d66f20d4bf8d7e238b27db
AmanKhys <amankhys2112@gmail.com>
2023-07-17 00:54:53 +0530

    normalizeURL and tests

---
+++ crawl.test.js
@@ -0,0 +1,25 @@
+const { normalizeURL } = require('./crawl.js')
+const { test, expect } = require('@jest/globals')
+
+test('normalizeURL strip protocol and trailing slash', () => {
+    const input = 'https://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+
+test('normalizeURL capital', () => {
+    const input = 'https://BLOG.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL strip http', () => {
+    const input = 'http://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
