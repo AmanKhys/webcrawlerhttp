e67b16c138f783dffc413f291b6207f87299f4b1
AmanKhys <amankhys2112@gmail.com>
2023-07-17 18:29:26 +0530

    crawl page recursive working

--- crawl.js
+++ crawl.js
@@ -1,24 +1,47 @@
 const {JSDOM} = require('jsdom')
 
-async function crawlPage(currrentURL) {
-    console.log(`actively crawling: ${currrentURL}`)
+async function crawlPage(baseURL, currentURL, pages) {
+    
+    const baseURLObj = new URL(baseURL)
+    const currentURLObj = new URL(currentURL)
+    if(baseURLObj.hostname !== currentURLObj.hostname) {
+        return pages 
+    }
+
+    const normalizedCurrentURL = normalizeURL(currentURL)
+    if(pages[normalizedCurrentURL]){
+        pages[normalizedCurrentURL]++;
+        return pages
+    }
+    pages[normalizedCurrentURL] = 1
+    console.log(`actively crawling: ${currentURL}`)
+
+
     try{
-        const resp = await fetch(currrentURL)
+        const resp = await fetch(currentURL)
         
         if(resp.status > 399) {
-            console.log(`error in fetch with status code: ${resp.status} on page: ${currrentURL}`)
-            return
+            console.log(`error in fetch with status code: ${resp.status} on page: ${currentURL}`)
+            return pages
         }
         const contentType = resp.headers.get("content-type")
         if(!contentType.includes("text/html")) {
-             console.log(`non html response, content type: ${contentType} on page: ${currrentURL}`)
-            return
+             console.log(`non html response, content type: ${contentType} on page: ${currentURL}`)
+            return pages
         }
-        console.log(await resp.text())
+
+        const htmlBody = await resp.text()
+
+        const nextURLs = getURLsFromHTML(htmlBody, baseURL)
+        
+        for(const nextURL of nextURLs){
+            pages = await crawlPage(baseURL, nextURL, pages)
+        }
     } catch(err) {
-        console.log(`error in fetch: ${err.message}, on page: ${currrentURL}`)
+        console.log(`error in fetch: ${err.message}, on page: ${currentURL}`)
     }
 
+    return pages
 }
 
 function normalizeURL(urlString) {
